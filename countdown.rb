93f0c47491da6574dce937da9fd50b2b70c098c9
Njerimacharia98 <njerimacharia05@gmail.com>
2023-07-05 19:32:21 +0300

    completed lab

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
 #write your code here
 
-def countdown
+def countdown (time)
+    while time > 0
+    puts "#{time} SECOND(S)!"
+    time -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(num)
+  while num > 0
+    puts "#{num} SECOND(S)!"
+    sleep(1) # Sleep for 1 second
+    num -= 1
+  end
+  "HAPPY NEW YEAR!"
 end
